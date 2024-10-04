bd725502434bd521f65f18ddd65bbccc02daa121
guyguy2001 <guygeva2311@gmail.com>
2024-10-04 22:33:23 +0300

    Refactor the main game loop, ruining :interaction-focus
    (assuming it wasn't broken in the previous commit)

--- src/clojure_adventure/core.clj
+++ src/clojure_adventure/core.clj
@@ -14,9 +14,9 @@
 
 
 (defn try-move-by
-  [grid entity by]
+  [board entity by]
   (let [moved (move-by entity by)]
-    (if (= (get-in grid [(:y moved) (:x moved)]) "-")
+    (if (= (get-in (:base-grid board) [(:y moved) (:x moved)]) "-")
       moved
       entity)))
 
@@ -27,8 +27,8 @@
    :down vec2/down})
 
 (defn enemy-turn
-  [grid enemy]
-  (update enemy :pos #(try-move-by grid % (rand-nth vec2/cardinal-directions))))
+  [board enemy]
+  (update enemy :pos #(try-move-by board % (rand-nth vec2/cardinal-directions))))
 
 
 ; TODO: I need to create an abstraction of the grid and the objects on it (layers).
@@ -55,49 +55,80 @@
 ; I think I wouldn't feel like I need this if I just used ->
 ; with functions that receive the state, and live outside of
 ; this as global functions
-(defn apply-to-state
-  "actions: [:player (fn[state] <new-player>)
-             :enemies (fn[state] <new-enemies>)]"
+(defn apply-to-objects
+  "actions: [:players (fn[state player] <new-player>)
+             :enemies (fn[state enemy] <new-enemiy>)]"
   [state actions]
-  (reduce (fn [state [key f]] (assoc state key (f state)))
-          state
-          (partition 2 actions)))
+  (update-in state [:board :objects]
+             (fn [objects]
+               (reduce (fn [objects [key f]] (assoc objects key (map (partial f state) (key objects))))
+                       objects
+                       (partition 2 actions)))))
+
+(comment
+  ; TODO:
+  ; make sure this function works
+  ; I just fucked :interaction-focus
+  ; I am sad now
+  ; I need to see how people refactor in clojure
+  (apply-to-objects
+   (get-initial-state)
+   [:enemies (fn [{:keys [board]} e] (enemy-turn board e))])
+  (def actions [:enemies (fn [{:keys [board]} e] (enemy-turn board e))])
+  (def state (get-initial-state))
+  (let [objects (get-in state [:board :objects])
+        ;; [key f] actions
+        key :enemies
+        objects {:enemies []}
+        f (fn [{:keys [board]} e] (println e))]
+    (assoc objects key (map (partial f state) objects))
+    (map (partial f state) objects))
+  :rcf)
+
+(defn update-with-context
+  "Like update, but the callback receives the entire map, not just the original value"
+  [map key f]
+  (assoc map key (f map)))
+
+(defn get-player
+  [board]
+  (first (:players (:objects board))))
+
+(defn get-new-interaction-focus
+  [{board :board}] ; param is `state`
+  (let [{:keys [base-grid objects]} board]
+    (get-interaction-focus-target base-grid objects (:pos (get-player board)))))
 
 (defn evaluate-turn
   [state_ input]
   (let [direction (get direction-by-input input)]
-    (apply-to-state
-     state_
-     [:player (fn [{:keys [player board]}]
-                (if (not= direction nil)
-                  (update player :pos #(try-move-by board % direction))
-                  player))
+    (-> state_
+        (apply-to-objects
+         [:players (fn [{:keys [board]} player]
+                     (if (not= direction nil)
+                       (update player :pos #(try-move-by board % direction))
+                       player))
 
-      :enemies
-      (fn [{:keys [board enemies]}] (map #(enemy-turn board %) enemies))
+          :enemies
+          (fn [{:keys [board]} enemy] (enemy-turn board enemy))])
 
-      :interaction-focus
-      (fn [{:keys [board objects player]}]
-        (get-interaction-focus-target board objects
-                                      (:pos player)))])))
+        (update-with-context
+         :interaction-focus
+         get-new-interaction-focus))))
 
 (comment
   (def state (get-initial-state))
+  (def direction (vec2/vec2 1 0))
   (def actions
-    (let [direction (get direction-by-input :right)]
-      [:player (fn [{:keys [player board]}]
-                 (if (not= direction nil)
-                   (update player :pos #(try-move-by board % direction))
-                   player))
-       :enemies (fn [{:keys [board enemies]}]
-                  (map #(enemy-turn board %) enemies))
-       :interaction-focus
-       (fn [{:keys [board objects player]}]
-         (get-interaction-focus-target board objects
-                                       (:pos player)))]))
+    [:player (fn [{:keys [board]} player]
+               (if (not= direction nil)
+                 (update player :pos #(try-move-by board % direction))
+                 player))
 
-  (apply-to-state state actions)
-
+     :enemies
+     (fn [{:keys [board]} enemy] (map #(enemy-turn board enemy)))])
+  (apply-to-objects state actions)
+  (get-initial-state)
   :rcf)
 
 (defn game-loop
@@ -119,13 +150,23 @@
 (defn get-initial-state
   []
   (let [board (get-initial-board)] ; TODO: this let is bad, and the board doesn't know the enemies aren't empty
-    {:board board
-     :player {:pos {:x 53 :y 15} :symbol "@"}
-     :enemies (population/populate-grid-return board "X" 5)
+    ; attempt at terminology: board = the "wolrd map", including all of the objects, grid = 2d arrady
+    ; walls and stuff like that are "dumb objects", since they don't have a payload (ther _is_ a wall in a certain spot, no more data than that).
+    ; players, enemis, etc are smart objects.
+    ; Question: the player lives in the board map because it is something that appears on the map.
+    ; The inventory doesn't live there because it doesn't appear on the map.
+    ; What gives?
+    ; Potential answer: the player is an entity that needs to execute logic each turn, the board isn't.
+    ; Potential answer: entities live on the board, resources live in the state.
+    ; Potential alternative - have the board contain pointers to the objects that are on it, and let them live outside of it.
+    ; I'm renaming`board` to `world` for now since this is getting out of freaking hand.
+    {:board {:base-grid board
+             :objects {:players [{:pos {:x 53 :y 15} :symbol "@"}] ; it's a singleton, but I want everything to be vecs I think.
+                       :enemies (population/populate-grid-return board "X" 5)
+                       :other [{:pos {:x 51 :y 13} :symbol "?"
+                                :name "Spellbook"}]}}
      :interaction-focus nil
-     :inventory {}
-     :objects [{:pos {:x 51 :y 13} :symbol "?"
-                :name "Spellbook"}]}))
+     :inventory {}}))
 
 (comment
   (def state (get-initial-state))
@@ -143,14 +184,14 @@
 
   :rcf)
 
-(defn get-debug-state
+(defn get-debug-state1
   []
   {:board (get-initial-board)
    :inventory {:iron 2 :wood 5}})
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
+  [& _args]
 
   (ui/with-screen
     (fn [screen]
@@ -161,3 +202,6 @@
   :rcf)
 ; DESIGN TODO:
 ; * how should objects be represented? stored?
+
+
+; I think that a lot of the issues I encountered during this refactor are caused by the fact that I accessed data directly instead of putting it behind abstractions - for example, by using get-in to get the data from the grid, instead of a grid/world abstraction, that would help me notice that I'm accessing the wrong thing, or would make it so I don't have the "wrong thing" to access in the first place
