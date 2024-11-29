c030da6cbbeb02b15dcb7ffa853345b817b87f61
guyguy2001 <guygeva2311@gmail.com>
2024-11-29 16:05:53 +0200

    Fix enemis going out of bounds

--- src/clojure_adventure/engine/collision.clj
+++ src/clojure_adventure/engine/collision.clj
@@ -1,5 +1,6 @@
 (ns clojure-adventure.engine.collision
-  (:require [clojure-adventure.world :as world]))
+  (:require [clojure-adventure.world :as world]
+            [clojure-adventure.grid :as grid]))
 
 (defn make-solid
   [entity]
@@ -10,10 +11,14 @@
   (:solid entity))
 
 ; TODO: I might want to change this to "would this entity get stuck if it went there"
+; TODO: Currently this includes the world's edges
 (defn is-pos-solid?
   [world pos]
-  (some -solid?
-        (world/get-objects-at-pos world pos)))
+  ; TODO: I maybe should have world implement the grid interface, for stuff like that
+  ; Or maybe the "has width/height" interface
+  (or (not (grid/in-bounds? (:new-grid world) pos))
+      (some -solid?
+            (world/get-objects-at-pos world pos))))
 
 (comment
   (require '[clojure-adventure.core :refer [initial-state]])
