9d18b25c71c89fa42e575ea6894d314904d649b4
guyguy2001 <guygeva2311@gmail.com>
2025-01-17 00:16:22 +0200

    Fix collision detection for stepping onto fireballs

--- src/clojure_adventure/movement.clj
+++ src/clojure_adventure/movement.clj
@@ -7,7 +7,8 @@
   [state entity-id by]
   (let [state (update state :world #(world/update-object % entity-id
                                                          assoc :facing-direction by))
-        target (vec2/add (:pos (world/get-object (:world state) entity-id)) by)]
+        target (vec2/add (:pos (world/get-object (:world state) entity-id)) by)
+        state (collision/notify-collision state entity-id target)]
     (if (collision/is-pos-solid? state target)
-      (collision/notify-collision state entity-id target)
+      state
       (update state :world #(world/move-to % entity-id target)))))
